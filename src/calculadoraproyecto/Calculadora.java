8bb2c5f07d64a2cdef9985ac4df198486f69c376
LautaroValle <lautarovallelopez@gmail.com>
2020-02-10 23:21:06 -0300

    creacion del label

--- src/calculadoraproyecto/Calculadora.java
+++ src/calculadoraproyecto/Calculadora.java
@@ -15,19 +15,29 @@ import javax.swing.*;
  */
 public class Calculadora extends JFrame{
     public Calculadora(){
-        this.setSize(500, 500);
+        this.setSize(500, 500);//Defino el tamaño de la ventana
         //this.setLocation(500, 0);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Defino al accion al cerrar la ventana
         this.setTitle("Calculadora");
         this.setLocationRelativeTo(null);
         this.initComponents();
     }
     
     private void initComponents(){
-        JPanel panel = new JPanel();
-        this.getContentPane().add(panel);
-        panel.setSize(400, 333);
-        panel.setForeground(Color.yellow);
+        //Editando el panel
+        JPanel panel = new JPanel();//Creo el panel
+        panel.setLayout(null);//Deshabilito los layouts predeterminados de las etiquetas
+        this.getContentPane().add(panel);//Agrego el panel a la ventana
+        
+        
+        //Editando el label
+        JLabel lbl = new JLabel("Hola mundo");//Creo una etiqueta label
+        lbl.setOpaque(true);
+        lbl.setSize(100, 10);
+        lbl.setBackground(Color.yellow);
+        panel.add(lbl);
+        
+        
         
     }
 }
